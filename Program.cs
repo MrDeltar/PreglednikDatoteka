5b18f95068aee095d68ef4ac77ae87be2a8ff4de
MrDeltar <44877756+MrDeltar@users.noreply.github.com>
2019-03-14 19:50:41 +0100

    Prikaz dostupnih logičkih diskova #7
    Dodano je prikazivanje dostupnih diskova/particija i informacija o njima (ime, ukupna velicina, zauzeti prostor, slobodni prostor, format).
    Ulazak u diskove i prikazivanje njihovih datoteka je WIP.

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DatotecniSustav01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****************************************************************\n***Ispisivanje svih diskova prisutnih (i spremnih) na Racunalu***");
+            DriveInfo[] diskovi = DriveInfo.GetDrives();
+            try
+            {
+                foreach (DriveInfo d in diskovi)
+                {
+                    if (d.IsReady == true)      // provjeravamo ako je disk spreman za koristenje
+                    {
+                        Console.WriteLine("Disk {0} ukupno {1}GB(slobodno {2}GB, zauzeto {4}GB); Format: {3}  ", d.Name, d.TotalSize / (1024 * 1024 * 1024), d.AvailableFreeSpace/(1024*1024*1024), d.DriveFormat, (d.TotalSize-d.AvailableFreeSpace)/(1024*1024*1024));
+                    }
+                }
+            }
+            catch (IOException e)       // za dohvat moguce greske
+            {
+                Console.WriteLine("Greska u dohvatu diska: {0}", e);
+            }
+            Console.WriteLine("*****************************************************************");
+
+
+            string direktorij = @"C:\";
+            DirectoryInfo dirInfo = new DirectoryInfo(direktorij);
+
+            var datoteke = dirInfo.GetFiles();
+            long velicina = 0;
+
+            Console.WriteLine("+------------------+-------------+---------+------------------------------------------+");
+            Console.WriteLine("| Veličina       B |          KB |      MB | Nazivi datoteka                          |");
+            Console.WriteLine("+------------------+-------------+---------+------------------------------------------+");
+            foreach (FileInfo d in datoteke)
+            {
+                velicina += d.Length;
+                Console.WriteLine("|{0, 15} B | {1, 8} KB | {2, 4} MB | {3,10} |", 
+                    d.Length, 
+                    d.Length / 1024, 
+                    d.Length / (1024 * 1024),
+                    d.FullName);
+            }
+            Console.WriteLine("+------------------+-------------+---------+------------------------------------------+");
+            Console.WriteLine("|{0, 15} B | {1, 8} KB | {2, 4} MB |                                          |",
+                velicina,
+                velicina / 1024,
+                velicina / (1024 * 1024));
+            Console.WriteLine("+------------------+-------------+---------+------------------------------------------+");
+
+            Console.SetCursorPosition(1, 3);
+            Console.Write(">");
+            int brojRedova = datoteke.Length + 6;
+
+            int cekanjeTreperenje = 500;
+            Console.CursorVisible = false;
+            int pokazivacY = 3;
+            while (true)
+            {
+                System.Threading.Thread.Sleep(cekanjeTreperenje);
+                Console.SetCursorPosition(1, pokazivacY);
+                Console.Write(" ");
+                System.Threading.Thread.Sleep(cekanjeTreperenje);
+                Console.SetCursorPosition(1, pokazivacY);
+                Console.Write(">");
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo pritisnutaTipka = Console.ReadKey(true);
+                    if (pritisnutaTipka.Key == ConsoleKey.DownArrow)
+                    {
+                        pokazivacY++;
+                    }
+                }
+            }
+
+            // Console.SetCursorPosition(0, brojRedova);
+        } //Main
+    }
+}
