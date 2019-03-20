3e30130b56857a5ab6fa5d07e59d9314fae5996f
MrDeltar <44877756+MrDeltar@users.noreply.github.com>
2019-03-20 14:50:38 +0100

    Dovrsena mogucnost odabira diska/particije

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace DatotecniSustav01
                 {
                     if (d.IsReady == true)      // provjeravamo ako je disk spreman za koristenje
                     {
-                        Console.WriteLine("Disk {0} ukupno {1}GB(slobodno {2}GB, zauzeto {4}GB); Format: {3}  ", d.Name, d.TotalSize / (1024 * 1024 * 1024), d.AvailableFreeSpace/(1024*1024*1024), d.DriveFormat, (d.TotalSize-d.AvailableFreeSpace)/(1024*1024*1024));
+                        Console.WriteLine("Disk {0} ukupno {1}GB(slobodno {2}GB, zauzeto {4}GB); Format: {3}", d.Name, d.TotalSize / (1024 * 1024 * 1024), d.AvailableFreeSpace / (1024 * 1024 * 1024), d.DriveFormat, (d.TotalSize - d.AvailableFreeSpace) / (1024 * 1024 * 1024));
                     }
                 }
             }
@@ -24,34 +24,71 @@ namespace DatotecniSustav01
             {
                 Console.WriteLine("Greska u dohvatu diska: {0}", e);
             }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine("Pristup disku je zabranjen: {0}", e);
+            }
+            Console.WriteLine("*****************************************************************");
+            @Console.WriteLine("Odaberi disk(npr. 'C:\\'):");
+            string odabir = Console.ReadLine();     // uneseni string MORA BITI ISTI kao ime zeljenog diska/particije, tj. odabir == d.Name
+            foreach (DriveInfo d in diskovi)
+            {
+                if (d.IsReady == true)
+                {
+                    try
+                    {
+                        if (d.ToString() == odabir)
+                        {
+                            string disk = d.ToString();
+                            DirectoryInfo dirInfo = new DirectoryInfo(disk);
+                            DirectoryInfo[] dir = dirInfo.GetDirectories();
+
+                            var datoteke = dirInfo.GetFiles();
+                            long velicina = 0;
+
+                            Console.WriteLine("+------------------+-------------+---------+------------------------------------------+");
+                            Console.WriteLine("| Veličina       B |          KB |      MB | Nazivi datoteka                          |");
+                            Console.WriteLine("+------------------+-------------+---------+------------------------------------------+");
+                            foreach (DirectoryInfo f in dir)
+                            {
+                                try
+                                {
+                                    Console.WriteLine("| {0 ,10} |", f.Name);
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine("Greska: {0}", e);
+                                }
+                            }
+                            foreach (FileInfo e in datoteke)
+                            {
+                                velicina += e.Length;
+                                Console.WriteLine("|{0, 15} B | {1, 8} KB | {2, 4} MB | {3,10} |",
+                                    e.Length,
+                                    e.Length / 1024,
+                                    e.Length / (1024 * 1024),
+                                    e.FullName);
+                            }
+                            Console.WriteLine("+------------------+-------------+---------+------------------------------------------+");
+                            Console.WriteLine("|{0, 15} B | {1, 8} KB | {2, 4} MB |                                          |",
+                                velicina,
+                                velicina / 1024,
+                                velicina / (1024 * 1024));
+                            Console.WriteLine("+------------------+-------------+---------+------------------------------------------+");
+
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Doslo je do greske: {0}", e);
+                    }
+                }
+            }
             Console.WriteLine("*****************************************************************");
 
+            Console.ReadLine();
 
-            string direktorij = @"C:\";
-            DirectoryInfo dirInfo = new DirectoryInfo(direktorij);
-
-            var datoteke = dirInfo.GetFiles();
-            long velicina = 0;
-
-            Console.WriteLine("+------------------+-------------+---------+------------------------------------------+");
-            Console.WriteLine("| Veličina       B |          KB |      MB | Nazivi datoteka                          |");
-            Console.WriteLine("+------------------+-------------+---------+------------------------------------------+");
-            foreach (FileInfo d in datoteke)
-            {
-                velicina += d.Length;
-                Console.WriteLine("|{0, 15} B | {1, 8} KB | {2, 4} MB | {3,10} |", 
-                    d.Length, 
-                    d.Length / 1024, 
-                    d.Length / (1024 * 1024),
-                    d.FullName);
-            }
-            Console.WriteLine("+------------------+-------------+---------+------------------------------------------+");
-            Console.WriteLine("|{0, 15} B | {1, 8} KB | {2, 4} MB |                                          |",
-                velicina,
-                velicina / 1024,
-                velicina / (1024 * 1024));
-            Console.WriteLine("+------------------+-------------+---------+------------------------------------------+");
-
+            /*
             Console.SetCursorPosition(1, 3);
             Console.Write(">");
             int brojRedova = datoteke.Length + 6;
@@ -77,7 +114,7 @@ namespace DatotecniSustav01
                     }
                 }
             }
-
+            */
             // Console.SetCursorPosition(0, brojRedova);
         } //Main
     }
